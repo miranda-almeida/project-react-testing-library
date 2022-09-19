4ed0c137df68b958b22742fd8c0c4bb263a846d7
miranda-almeida <magui@duck.com>
2022-09-19 17:34:01 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('Testa o componente NotFound.js', () => {
+  it('Verifica h2 com texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+
+    const heading = screen.getByRole('heading', {
+      name: 'Page requested not found',
+      level: 2,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Verifica exibição de gif', () => {
+    renderWithRouter(<NotFound />);
+
+    const img = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+    expect(img).toBeInTheDocument();
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
